19f97c03f4cb5fec8f5babdf4801c1a481b5b0b9
takshamrutiya07 <takshamrutiya@gmail.com>
2025-12-04 22:21:41 +0530

    Leetcode Question:485

---
+++ MaxConsecutiveOnes.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+            {
+                count++;
+            }else{
+                (maxi>count)?maxi=maxi:maxi=count;
+                count=0;
+            }
+        }
+        return (maxi>count)? maxi:count;
+    }
+};
+int main() {
+    Solution s;
+    vector<int>v={1,1,0,1,1,1};
+    cout<<s.findMaxConsecutiveOnes(v);
+    return 0;
+}
