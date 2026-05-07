58aa3c779e31e57eccda0092ba2d7e6acf600dba
takshamrutiya07 <takshamrutiya@gmail.com>
2026-05-07 15:16:49 +0530

    Leetcode Question : 1002

---
+++ 1002_Find_Common_Characters.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        sort(words.begin(),words.end());
+        for(auto i:words)
+        {
+            
+        }
+        
+    }
+};
+
+int main() {
+    Solution s;
+    vector<string>str={"roller","bella","label"};
+    s.commonChars(str);
+    return 0;
+}
