fe45290d433e1d27f7086f77e916c4a58012ae6e
takshamrutiya07 <takshamrutiya@gmail.com>
2026-08-02 15:05:17 +0530

    Leetcode Question:877

---
+++ 877_Stone_Game.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int len = 2; len <= n; len++) {
+            for (int l = 0; l + len - 1 < n; l++) {
+                int r = l + len - 1;
+
+                dp[l][r] = max(
+                    piles[l] - dp[l + 1][r],
+                    piles[r] - dp[l][r - 1]
+                );
+            }
+        }
+        return dp[0][n - 1] > 0;
+    }
+};
+int main() {
+    Solution s;
+    vector<int>piles={5,3,4,5};
+    cout<<s.stoneGame(piles);
+    return 0;
+}
