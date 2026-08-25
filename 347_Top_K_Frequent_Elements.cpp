3a9ce64cc2e423585bea13accb4540b53cba075d
takshamrutiya07 <takshamrutiya@gmail.com>
2026-08-25 15:08:10 +0530

    Leetcode Question:347

--- 347_Top_K_Frequent_Elements.cpp
+++ 347_Top_K_Frequent_Elements.cpp
@@ -3,38 +3,50 @@ using namespace std;
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int>ans;
-        unordered_map<int ,int>mp;
-        for(auto i:nums)
+
+        vector<int> ans;
+        unordered_map<int, int> mp;
+
+        for(auto i : nums)
         {
             mp[i]++;
-        } 
-        vector<pair<int,int>>v;
+        }
+
+        vector<pair<int,int>> v;
+
         for(auto j : mp)
         {
             v.push_back({j.first, j.second});
         }
-        sort(v.begin(),v.end(),[](auto a, auto b)
+
+        sort(v.begin(), v.end(), [](auto a, auto b)
         {
             return a.second > b.second;
         });
-        for(int i=0;i<k;i++)
+
+        for(int i = 0; i < k; i++)
         {
             ans.push_back(v[i].first);
         }
+
         return ans;
     }
 };
 
 int main() {
+
     Solution s;
-    vector<int>v={1,1,1,2,2,3};
-    vector<int>ans;
+
+    vector<int> v = {1,1,1,2,2,3};
+
     int k = 2;
-    ans = s.topKFrequent(v,k);
-    for(auto i:ans)
+
+    vector<int> ans = s.topKFrequent(v, k);
+
+    for(auto i : ans)
     {
-        cout<<i<<" ";
+        cout << i << " ";
     }
+
     return 0;
 }
